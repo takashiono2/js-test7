26ea9c50f161d7b9df78171e717accba4dd6ebad
takashiono2 <takashiono710@gmail.com>
2021-07-01 12:35:19 +0900

    クリックイベントを設定しよう

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 'use strict';
 {
-  
+  const dts = document.querySelectorAll('dt');
+  dts.forEach(dt=>{
+    dt.addEventListener('click',()=>{
+      dt.parentNode.classList.toggle('appear');
+    });
+  });
 }

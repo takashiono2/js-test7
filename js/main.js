502b5f33fc74809d49c4d8147aa5cd7f2a76973f
takashiono2 <takashiono710@gmail.com>
2021-07-01 15:15:43 +0900

    ひとつ開いたら他を閉じる実装をしよう

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@
   dts.forEach(dt=>{
     dt.addEventListener('click',()=>{
       dt.parentNode.classList.toggle('appear');
-    });
+      // dt.parentElement.classList.toggle('appear');
+  dts.forEach(el=>{
+    if( el!== dt ){
+      el.parentNode.classList.remove('appear');
+      // el.parentElement.classList.remove('appear');
+    }
+  });
+  });
   });
 }
